dce21af9f6639bcbffbab0d7dd5ecec7b83aefa1
Uvaacha <info@uvaacha.co.in>
2025-12-31 17:03:11 +0530

    updated

--- frontend/holland-flowers/src/App.js
+++ frontend/holland-flowers/src/App.js
@@ -51,7 +51,7 @@ import OrderHistory from './components/OrderHistory';
 import OrderDetail from './components/OrderDetail';
 import RefundPolicy from './components/RefundPolicy';
 import PrivacyPolicy from './components/PrivacyPolicy';
-import NewYearPopup from './components/NewYearPopup';
+import NewYearCelebration from './components/NewYearCelebration';
 import './App.css';
 
 // ============================================
@@ -1083,7 +1083,7 @@ const AppContent = ({ currentLang }) => {
     <div className={`app ${currentLang === 'ar' ? 'rtl' : 'ltr'}`}>
       <ScrollToTop />
       <Header />
-      <NewYearPopup />
+      <NewYearCelebration />
       
       <Routes>
         <Route path="/" element={<HomePage currentLang={currentLang} />} />
