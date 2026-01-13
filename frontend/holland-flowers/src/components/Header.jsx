b5d72b2892bb3b0c1838a93e9d1d2bb7dd83aea9
Uvaacha <info@uvaacha.co.in>
2026-01-13 13:57:43 +0530

    Updated the code for logo mismatch the name

--- frontend/holland-flowers/src/components/Header.jsx
+++ frontend/holland-flowers/src/components/Header.jsx
@@ -471,7 +471,7 @@ const Header = () => {
 
           <Link to="/" className="logo" onClick={handleLinkClick}>
             <img 
-              src="/Holland-Logo.png" 
+              src="/Holland-logo.png" 
               alt={currentLang === 'ar' ? 'فلورا كويت' : 'Flora Kuwait'} 
               className="logo-image"
             />
